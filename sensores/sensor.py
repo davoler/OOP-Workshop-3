8ede9c8a40cfe7e1a1f7cc769a92ab770e949e29
angiepandales <angie.pandales@ude.edu.co>
2025-06-24 14:03:16 -0500

    cambios

---
+++ sensores/sensor.py
@@ -0,0 +1,39 @@
+# sensor.py
+from Sistemas.base_subsystem import BaseSubsystem
+import random
+
+class Gyroscope(BaseSubsystem):
+    def __init__(self, name: str = "Gyroscope"):
+        super().__init__(name)
+    
+    def initialize(self):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def read_data(self):
+        return {
+            "omega_x": round(random.uniform(-0.01, 0.01), 4),
+            "omega_y": round(random.uniform(-0.01, 0.01), 4),
+            "omega_z": round(random.uniform(-0.01, 0.01), 4)
+        }
+
+class SunSensor(BaseSubsystem):
+    def __init__(self, name: str = "SunSensor"):
+        super().__init__(name)
+    
+    def initialize(self):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def read_data(self):
+        # Simula ángulo hacia el Sol en elevación y azimut
+        elevation = round(random.uniform(-90, 90), 2)
+        azimuth = round(random.uniform(0, 360), 2)
+        return {
+            "sun_elevation": elevation,
+            "sun_azimuth": azimuth
+        }
