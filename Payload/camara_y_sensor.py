2b54532f2b8d5e8c2d19e791fe33c62461ccb311
eimy05 <eimy.trejos@udea.edu.co>
2025-06-24 13:20:20 -0500

    Anexo payload a rama main

---
+++ Payload/camara_y_sensor.py
@@ -0,0 +1,11 @@
+from Sistemas.base_subsystem import BaseSubsystem
+
+class TermalCam(BaseSubsystem):
+    def _init_(self, name: str = "Termal Camara"):
+        super()._init_(name)
+    
+    def initialize(self):
+        pass
+
+    def update(self, dt):
+        pass
