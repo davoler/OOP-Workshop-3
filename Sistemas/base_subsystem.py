86453157e603943f2175f9ff363f58fdfd353ea7
davoler <david.cubillos@udea.edu.co>
2025-06-24 11:21:15 -0500

    Creación clase abstracta subsistemas

---
+++ Sistemas/base_subsystem.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class BaseSubsystem(ABC):
+    
+    def __init__(self, name: str):
+        
+        self.name = name
+        self.active = True 
+
+    @abstractmethod
+    def initialize(self):
+        pass
+
+    @abstractmethod
+    def update(self, dt: float):
+        pass
+
